de7c864c80ecda70c7956d099aafb717448cb219
mrjs <mr.js.programmer@gmail.com>
2024-06-17 21:54:01 +0330

    add : download with pdf , word , excel

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,16 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import { TextField, Button, Typography, Paper } from '@material-ui/core';
-import { useDispatch, useSelector } from 'react-redux';
-import FileBase from 'react-file-base64';
+import React, { useState, useEffect } from "react";
+import { TextField, Button, Typography, Paper } from "@material-ui/core";
+import { useDispatch, useSelector } from "react-redux";
+import FileBase from "react-file-base64";
+import * as XLSX from "xlsx";
+import jsPDF from "jspdf";
+import { Document, Packer, Paragraph, TextRun } from "docx";
+import { saveAs } from "file-saver";
 
-import useStyles from './styles';
-import { createPost, updatePost } from '../../actions/posts';
+import useStyles from "./styles";
+import { createPost, updatePost } from "../../actions/posts";
 
 const Form = ({ currentId, setCurrentId }) => {
-  const [postData, setPostData] = useState({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
-  const post = useSelector((state) => (currentId ? state.posts.find((message) => message._id === currentId) : null));
+  const [postData, setPostData] = useState({
+    creator: "",
+    title: "",
+    message: "",
+    tags: "",
+    selectedFile: "",
+  });
+  const post = useSelector((state) =>
+    currentId ? state.posts.find((message) => message._id === currentId) : null
+  );
   const dispatch = useDispatch();
   const classes = useStyles();
+  const posts = useSelector((state) => state.posts);
 
   useEffect(() => {
     if (post) setPostData(post);
@@ -18,7 +31,13 @@ const Form = ({ currentId, setCurrentId }) => {
 
   const clear = () => {
     setCurrentId(0);
-    setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
+    setPostData({
+      creator: "",
+      title: "",
+      message: "",
+      tags: "",
+      selectedFile: "",
+    });
   };
 
   const handleSubmit = async (e) => {
@@ -34,29 +53,153 @@ const Form = ({ currentId, setCurrentId }) => {
   };
 
   const downloadpdf = () => {
-    console.log("test");
-  }
+    const doc = new jsPDF();
+
+    posts.forEach((item, index) => {
+      doc.text(`Title: ${item.title}`, 10, 10 + index * 10);
+      doc.text(`Message: ${item.message}`, 10, 20 + index * 10);
+    });
+
+    doc.save("data.pdf");
+  };
   const downloadexcel = () => {
-    console.log("test");
-  }
+    const worksheet = XLSX.utils.json_to_sheet(posts);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+
+    XLSX.writeFile(workbook, "data.xlsx");
+  };
   const downloadword = () => {
-    console.log("test");
-  }
+    const doc = new Document({
+      sections: [
+        {
+          children: posts.map(
+            (item) =>
+              new Paragraph({
+                children: [
+                  new TextRun(`Title: ${item.title}`),
+                  new TextRun(`Message: ${item.message}`),
+                ],
+              })
+          ),
+        },
+      ],
+    });
+
+    Packer.toBlob(doc).then((blob) => {
+      saveAs(blob, "data.docx");
+    });
+  };
 
   return (
     <Paper className={classes.paper}>
-      <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-        <Typography variant="h6">{currentId ? `Editing "${post.title}"` : 'Creating a Memory'}</Typography>
-        <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
-        <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
-        <TextField name="message" variant="outlined" label="Message" fullWidth multiline rows={4} value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
-        <TextField name="tags" variant="outlined" label="Tags (coma separated)" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })} />
-        <div className={classes.fileInput}><FileBase type="file" multiple={false} onDone={({ base64 }) => setPostData({ ...postData, selectedFile: base64 })} /></div>
-        <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
-        <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
-        <Button variant="contained" color="default" size="small" style={{marginTop : "12px"}} onClick={downloadpdf} fullWidth>Download PDF</Button>
-        <Button variant="contained" color="default" size="small" style={{marginTop : "12px"}} onClick={downloadexcel} fullWidth>Download Excel</Button>
-        <Button variant="contained" color="default" size="small" style={{marginTop : "12px"}} onClick={downloadword} fullWidth>Download Word</Button>
+      <form
+        autoComplete="off"
+        noValidate
+        className={`${classes.root} ${classes.form}`}
+        onSubmit={handleSubmit}
+      >
+        <Typography variant="h6">
+          {currentId ? `Editing "${post.title}"` : "Creating a Memory"}
+        </Typography>
+        <TextField
+          name="creator"
+          variant="outlined"
+          label="Creator"
+          fullWidth
+          value={postData.creator}
+          onChange={(e) =>
+            setPostData({ ...postData, creator: e.target.value })
+          }
+        />
+        <TextField
+          name="title"
+          variant="outlined"
+          label="Title"
+          fullWidth
+          value={postData.title}
+          onChange={(e) => setPostData({ ...postData, title: e.target.value })}
+        />
+        <TextField
+          name="message"
+          variant="outlined"
+          label="Message"
+          fullWidth
+          multiline
+          rows={4}
+          value={postData.message}
+          onChange={(e) =>
+            setPostData({ ...postData, message: e.target.value })
+          }
+        />
+        <TextField
+          name="tags"
+          variant="outlined"
+          label="Tags (coma separated)"
+          fullWidth
+          value={postData.tags}
+          onChange={(e) =>
+            setPostData({ ...postData, tags: e.target.value.split(",") })
+          }
+        />
+        <div className={classes.fileInput}>
+          <FileBase
+            type="file"
+            multiple={false}
+            onDone={({ base64 }) =>
+              setPostData({ ...postData, selectedFile: base64 })
+            }
+          />
+        </div>
+        <Button
+          className={classes.buttonSubmit}
+          variant="contained"
+          color="primary"
+          size="large"
+          type="submit"
+          fullWidth
+        >
+          Submit
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          size="small"
+          onClick={clear}
+          fullWidth
+        >
+          Clear
+        </Button>
+        <Button
+          variant="contained"
+          color="default"
+          size="small"
+          style={{ marginTop: "12px" }}
+          onClick={downloadpdf}
+          fullWidth
+        >
+          Download PDF
+        </Button>
+        <Button
+          variant="contained"
+          color="default"
+          size="small"
+          style={{ marginTop: "12px" }}
+          onClick={downloadexcel}
+          fullWidth
+        >
+          Download Excel
+        </Button>
+        <Button
+          variant="contained"
+          color="default"
+          size="small"
+          style={{ marginTop: "12px" }}
+          onClick={downloadword}
+          fullWidth
+        >
+          Download Word
+        </Button>
       </form>
     </Paper>
   );
